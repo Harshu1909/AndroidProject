eaedf9d376e451d81cc2b67553b832535a7c58bf
slee4354 <slee4354@conestogac.on.ca>
2018-06-18 16:46:22 -0400

    ViewAccount, UpdateAccount, BuyItem from Sumin

---
+++ Assignment1/app/src/main/java/com/example/harshpatel/assignment1/ViewAccount.java
@@ -0,0 +1,36 @@
+package com.example.harshpatel.assignment1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ViewAccount extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_account);
+        String firstName = getIntent().getStringExtra("firstName");
+        String lastName = getIntent().getStringExtra("lastName");
+        String UserName = getIntent().getStringExtra("username");
+        String Gender = getIntent().getStringExtra("gender");
+        //(EditText)findViewById(R.id.Fname).set
+    }
+    public void update(View view) {
+
+        Intent updateAccount =  new Intent(ViewAccount.this,UpdateAccount.class);
+        startActivity(updateAccount);
+    }
+
+
+    public void delete(View view) {
+
+        Toast.makeText(this, "Successfully delete Account.", Toast.LENGTH_SHORT).show();
+        Intent viewitemlist = new Intent(ViewAccount.this,ViewItemList.class);
+        startActivity(viewitemlist);
+    }
+}
+
+
