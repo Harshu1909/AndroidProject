6984f97ef48729de05b864929aa9fde64da76c17
Harsh Patel <harshpatel@Harshs-MacBook-Pro.local>
2018-06-18 17:36:16 -0400

    View item details completed

--- Assignment1/app/src/main/java/com/example/harshpatel/assignment1/ViewItemList.java
+++ Assignment1/app/src/main/java/com/example/harshpatel/assignment1/ViewItemList.java
@@ -1,7 +1,9 @@
 package com.example.harshpatel.assignment1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ViewItemList extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class ViewItemList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_item_list);
+    }
+
+    public void FirstItem(View view) {
+
+        Intent firstitem = new Intent(ViewItemList.this,ViewItemDetails.class);
+        startActivity(firstitem);
     }
 }
