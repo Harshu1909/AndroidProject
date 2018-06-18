57284a7784b0bfc145611b9dc4fe7d54bec39abc
Harsh Patel <harshpatel@Harshs-MacBook-Pro.local>
2018-06-18 12:49:46 -0400

    Login/Registration Event

--- Assignment1/app/src/main/java/com/example/harshpatel/assignment1/MainActivity.java
+++ Assignment1/app/src/main/java/com/example/harshpatel/assignment1/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.harshpatel.assignment1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText username,password;
+    Button login,clear;
+
+    String UsernameString,PasswordString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        login = (Button)findViewById(R.id.login);
+        clear = (Button)findViewById(R.id.clear);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                UsernameString = username.getText().toString();
+                PasswordString = password.getText().toString();
+
+                if (UsernameString.equals("Admin") && PasswordString.equals("Admin")){
+
+                    Intent viewitemlist = new Intent(MainActivity.this,ViewItemList.class);
+                    startActivity(viewitemlist);
+                }
+                else{
+
+                    Toast.makeText(MainActivity.this, "Invalid Username/Password", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username.setText("");
+                password.setText("");
+            }
+        });
+
+
+
+    }
+
+    public void registration(View view) {
+
+        Intent registration =  new Intent(MainActivity.this,registration.class);
+        startActivity(registration);
     }
 }
--- Assignment1/app/src/main/java/com/example/harshpatel/assignment1/registration.java
+++ Assignment1/app/src/main/java/com/example/harshpatel/assignment1/registration.java
@@ -2,12 +2,39 @@ package com.example.harshpatel.assignment1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class registration extends AppCompatActivity {
+
+    EditText Fname,Lname,Username,Password,Cpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        Fname = (EditText)findViewById(R.id.Fname);
+        Lname = (EditText)findViewById(R.id.Lname);
+        Username = (EditText)findViewById(R.id.Username);
+        Password = (EditText)findViewById(R.id.Password);
+        Cpassword = (EditText)findViewById(R.id.Cpassword);
+
+    }
+
+    public void register(View view) {
+
+        Toast.makeText(this, "New User has been register", Toast.LENGTH_SHORT).show();
+    }
+
+    public void clear(View view) {
+
+        Fname.setText("");
+        Lname.setText("");
+        Username.setText("");
+        Password.setText("");
+        Cpassword.setText("");
+
     }
 }
