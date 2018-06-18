5b4f8aa631dcc254554acaeab93ba5ed3df7256d
Harsh Patel <harshpatel@Harshs-MacBook-Pro.local>
2018-06-18 18:49:40 -0400

    Checkout page

--- Assignment1/app/src/main/java/com/example/harshpatel/assignment1/ViewItemDetails.java
+++ Assignment1/app/src/main/java/com/example/harshpatel/assignment1/ViewItemDetails.java
@@ -1,7 +1,9 @@
 package com.example.harshpatel.assignment1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ViewItemDetails extends AppCompatActivity {
 
@@ -10,4 +12,19 @@ public class ViewItemDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_item_details);
     }
+
+    public void checkoutItem(View view) {
+
+        Intent checkoutItem = new Intent(ViewItemDetails.this,CheckoutItem.class);
+        startActivity(checkoutItem);
+    }
+
+    public void cancel(View view) {
+
+        Intent cancel = new Intent(ViewItemDetails.this,ViewItemList.class);
+        startActivity(cancel);
+        this.finish();
+    }
+
+
 }
